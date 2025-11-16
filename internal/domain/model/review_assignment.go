1785f80b455c4c537c551655506c6ac2ee4225b1
Gregory Tolmachev <grtollm@gmail.com>
2025-11-16 12:47:32 +0300

    fix(model): change ID to PullRequestID

--- internal/domain/model/review_assignment.go
+++ internal/domain/model/review_assignment.go
@@ -1,14 +1,11 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"time"
 )
 
-type ReviewAssignmentID uuid.UUID
-
 type ReviewAssignment struct {
-	ID         ReviewAssignmentID `db:"id"`
-	ReviewerID UserID             `db:"reviewer_id"`
-	AssignedAt time.Time          `db:"assigned_at"`
+	PullRequestID PullRequestID `db:"pull_request_id"`
+	ReviewerID    UserID        `db:"reviewer_id"`
+	AssignedAt    time.Time     `db:"assigned_at"`
 }
