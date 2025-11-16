c1cd6829cb65e8f3a513a208e312c861ef284097
Gregory Tolmachev <grtollm@gmail.com>
2025-11-16 12:44:15 +0300

    fix: fix order of drops for pull_requests and fix review assignments

--- db/migrations/003_create_pull_requests.down.sql
+++ db/migrations/003_create_pull_requests.down.sql
@@ -1,2 +1,2 @@
-DROP TYPE IF EXISTS pull_request_status;
 DROP TABLE IF EXISTS pull_requests;
+DROP TYPE IF EXISTS pull_request_status;
--- db/migrations/004_create_review_assignments.up.sql
+++ db/migrations/004_create_review_assignments.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE review_assignments(
     pull_request_id UUID NOT NULL REFERENCES pull_requests(pull_request_id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-    assigned_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    assigned_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     PRIMARY KEY (pull_request_id, user_id)
 )
